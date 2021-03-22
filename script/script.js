46485c2778dd32e520004f9ef7fd274a12655a2a
IsaiahHenry <isaiahhenryb@gmail.com>
2021-03-22 13:09:30 -0400

    complete

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+function buyStuff(){
 let name = prompt(`What's your name?`)
 let orderOne = prompt(`Alright, ${name} what is the first item?`)
 let priceOne = Number(prompt(`So how much is/are the ${orderOne}?`))
@@ -21,7 +22,18 @@ if (isNaN(priceThree)){
     priceThree = Number(prompt(`So how much is/are the ${orderThree}?`))
 }
 // I found out about while, but i don't want to use it here in place of the 'if's. But it's so coooool (;n;) 
-let allPrices = priceOne + priceTwo + priceThree
-let grandTotal = (allPrices * 0.0625)+(allPrices)
+function addPrices(priceOne,priceTwo,priceThree){
+    return priceOne + priceTwo + priceThree;
+}
+// console.log(addPrices(priceOne,priceTwo,priceThree))
+let allPrices = addPrices(priceOne,priceTwo,priceThree)
+
+function grandTotal(allprices){
+    return  (allPrices * 0.0625)+ allPrices;
+}
+
+// let allPrices = priceOne + priceTwo + priceThree
+// let grandTotal = (allPrices * 0.0625)+(allPrices)
 document.getElementById("name").innerHTML = `This is what you wanted, ${name}.`
-document.getElementById("price").innerHTML = `Your grand total is $${allPrices.toFixed(2)} plus tax which is $${grandTotal.toFixed(2)}`
+document.getElementById("price").innerHTML = `Your total is $${addPrices(priceOne,priceTwo,priceThree).toFixed(2)} plus tax which is $${grandTotal().toFixed(2)}`
+}
